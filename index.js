4cd7b2342336601b5e20a67de9b931f9dd885c42
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2024-03-08 19:41:47 +0200

    timer

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const targetDate = new Date("Jul 17, 2024");
-const day = document.querySelector('[data-value="days"]');
-const hour = document.querySelector('[data-value="hours"]');
-const min = document.querySelector('[data-value="mins"]');
-const sec = document.querySelector('[data-value="secs"]');
+const day = document.querySelector(`[data-value="days"]`);
+const hour = document.querySelector(`[data-value="hours"]`);
+const min = document.querySelector(`[data-value="mins"]`);
+const sec = document.querySelector(`[data-value="secs"]`);
 
 function startTimer() {
   const time = targetDate - new Date();

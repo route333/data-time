0f6d558d9d8468a2a50154fac7e654cd7a065da1
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2024-03-09 10:47:33 +0200

    timer

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const targetDate = new Date("Jul 17, 2024");
-const day = document.querySelector(`[data-value="days"]`);
-const hour = document.querySelector(`[data-value="hours"]`);
-const min = document.querySelector(`[data-value="mins"]`);
-const sec = document.querySelector(`[data-value="secs"]`);
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
 
-function startTimer() {
-  const time = targetDate - new Date();
+    this.timer();
+  }
 
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  timer() {
+    this.timerElements = {
+      days: document.querySelector(`${this.selector} [data-value="days"]`),
+      hours: document.querySelector(`${this.selector} [data-value="hours"]`),
+      mins: document.querySelector(`${this.selector} [data-value="mins"]`),
+      secs: document.querySelector(`${this.selector} [data-value="secs"]`),
+    };
+    
+    this.timerInterval = setInterval(() => {
+      this.startTimer()
+    }, 1000);
+  }
 
-  day.textContent = days < 10 ? "0" + days : days;
-  hour.textContent = hours < 10 ? "0" + hours : hours;
-  min.textContent = mins < 10 ? "0" + mins : mins;
-  sec.textContent = secs < 10 ? "0" + secs : secs;
+  startTimer() {
+    const time = this.targetDate - new Date();
+
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    this.timerElements.days.textContent = days < 10 ? "0" + days : days;
+    this.timerElements.hours.textContent = hours < 10 ? "0" + hours : hours;
+    this.timerElements.mins.textContent = mins < 10 ? "0" + mins : mins;
+    this.timerElements.secs.textContent = secs < 10 ? "0" + secs : secs;
+  }
 }
 
-const timerInterval = setInterval(startTimer, 1000);
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2024"),
+});
